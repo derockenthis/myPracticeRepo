dcc5925183d0b527e24b309525caeb2ef9894a12
derockenthis <derekin00@gmail.com>
2022-09-15 11:03:43 -0700

    push after merge conflict

--- git-exercise/src/ApplicationMain.java
+++ git-exercise/src/ApplicationMain.java
@@ -4,26 +4,17 @@ public class ApplicationMain {
 		String[] shopList = new String[] {
 				"Apples",
 				"Bananas",
-<<<<<<< HEAD
 				"Cucumbers",
-				"Cat Food",
-=======
-				"Corn",
-				"More Dog Food",
->>>>>>> branch 'dev' of https://github.com/derockenthis/myPracticeRepo.git
+				"Cat Food and More Dog Food",
 				"Cereal", 
 				"Bread", 
 				"Eggs",
 				"Milk"
 		};
-<<<<<<< HEAD
 		
-		
-=======
 		System.out.println("My Shopping List");
              	System.out.println();
-
->>>>>>> branch 'dev' of https://github.com/derockenthis/myPracticeRepo.git
+             	
 		for (String item: shopList) {
 			System.out.println(item);
 		}	
